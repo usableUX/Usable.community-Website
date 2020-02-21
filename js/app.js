d8a3c7e1236cf1533dcc390f06e4afae93d3b350
Kene Udeze <me@udezekene.com>
2020-02-21 09:59:39 +0100

    Another push to make it live

--- js/app.js
+++ js/app.js
@@ -30,4 +30,48 @@ window.twttr = (function(d, s, id) {
         };
 
         return t;
-    }(document, "script", "twitter-wjs"));
+}(document, "script", "twitter-wjs"));
+
+// Google Analytics
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-31913096-19', 'auto');
+
+// Replace the following lines with the plugins you want to use.
+ga('require', 'eventTracker');
+ga('require', 'outboundLinkTracker');
+ga('require', 'urlChangeTracker');
+
+
+/*
+    {{Mobile Menu Navigation Toggle}}
+*/
+//
+// document.getElementById('mobileMenu').addEventListener('click', function(e){
+//     e.preventDefault();
+//
+//     var mobileMenu = document.getElementById('mobileMenu');
+//     var mobileNavigation = document.getElementById('mobileNavigation');
+//     mobileMenu.classList.toggle('is-active');
+//     mobileMenu.classList.toggle('is-active');
+//
+// });
+
+// querySelector returns the first element it finds with the correct selector
+// so it needs a unique class name or you're only grabbing one element
+
+// document.querySelector('.header__navigation__button').addEventListener('click', function(e) {
+//   [].map.call(document.querySelectorAll('.header__navigation__mobile, .header__navigation__bars, .header__navigation__button'), function(el) {
+//
+//     el.classList.toggle('is-active');
+//   });
+//
+// });
+
+
+// Gave up writing vanilla and went with jQuery
+$(document).ready(function() {
+    $('.header__navigation__button').on('click', function(e){
+        e.preventDefault();
+        $('.header__navigation__mobile, .header__navigation__bars, .header__navigation__button').toggleClass('is-active')
+    });
+});
