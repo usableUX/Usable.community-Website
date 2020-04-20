52761c3e303261b1d091500aef9a1a3b549f21a2
Kene Udeze <me@udezekene.com>
2020-04-20 20:33:23 +0200

    A hero section that works

--- js/app.js
+++ js/app.js
@@ -2,17 +2,18 @@
 // import Typewriter from 'typewriter-effect/dist/core';
 //var typespace = document.getElementsById('');
 // import Typed from 'typed.js';
-
 var options = {
-  stringsElement: '#landing__copy__typer_pool',
-    typeSpeed: 40,
-    backSpeed: 10,
+    stringsElement: '#landing__ticker__pool',
+    smartBackspace: true,
+    typeSpeed: 80,
+    showCursor: false,
+    backSpeed: 40,
     backDelay: 1200,
     startDelay: 500,
     loop: true,
 };
 
-var typed = new Typed('.landing__copy__typer', options);
+var typed = new Typed('.landing__ticker__slot', options);
 
 //Initialize Twitter
 window.twttr = (function(d, s, id) {
